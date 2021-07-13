1e735fa53e6115f9ed8aad32eca3b2d817d34307
Jivko Uzunov <jivkou@yahoo.com>
2021-07-13 17:45:44 +0300

    add spring data intro

---
+++ SpringDataIntro/src/main/java/com/example/springintro/CommandLineRunnerImpl.java
@@ -0,0 +1,81 @@
+package com.example.springintro;
+
+import com.example.springintro.model.entity.Book;
+import com.example.springintro.service.AuthorService;
+import com.example.springintro.service.BookService;
+import com.example.springintro.service.CategoryService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+    private final CategoryService categoryService;
+    private final AuthorService authorService;
+    private final BookService bookService;
+
+    public CommandLineRunnerImpl(CategoryService categoryService, AuthorService authorService, BookService bookService) {
+        this.categoryService = categoryService;
+        this.authorService = authorService;
+        this.bookService = bookService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        seedData();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of task:");
+        String task = sc.nextLine();
+        switch (task){
+            case "1":
+                printAllBooksAfterYear(2000);
+                break;
+            case "2":
+                printAllAuthorsNamesWithBooksWithReleaseDateBeforeYear(1990);
+                break;
+            case "3":
+                printAllAuthorsAndNumberOfTheirBooks();
+                break;
+            case "4":
+                pritnALlBooksByAuthorNameOrderByReleaseDate("George", "Powell");
+                break;
+        }
+
+    }
+
+    private void pritnALlBooksByAuthorNameOrderByReleaseDate(String firstName, String lastName) {
+        bookService
+                .findAllBooksByAuthorFirstAndLastNameOrderByReleaseDate(firstName, lastName)
+                .forEach(System.out::println);
+    }
+
+    private void printAllAuthorsAndNumberOfTheirBooks() {
+        authorService
+                .getAllAuthorsOrderByCountOfTheirBooks()
+                .forEach(System.out::println);
+    }
+
+    private void printAllAuthorsNamesWithBooksWithReleaseDateBeforeYear(int year) {
+        bookService
+                .findAllAuthorsWithBooksWithReleaseDateBeforeYear(year)
+                .forEach(System.out::println);
+    }
+
+    private void printAllBooksAfterYear(int year) {
+        bookService
+                .findAllBooksAfterYear(year)
+                .stream()
+                .map(Book::getTitle)
+                .forEach(System.out::println);
+    }
+
+    private void seedData() throws IOException {
+        categoryService.seedCategories();
+        authorService.seedAuthors();
+        bookService.seedBooks();
+    }
+}
